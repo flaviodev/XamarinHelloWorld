0de771b34f140ad3f00f337f68fc5c6526ae7089
flaviodev <fdsdev@gmail.com>
2017-10-28 14:57:20 -0200

    initial commit

---
+++ App1/App1/Page1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App1.logica;
+
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+
+            BtnSoma.Clicked += delegate
+            {
+                int num1 = int.Parse(Numero1.Text);
+                int num2 = int.Parse(Numero2.Text);
+
+                int resultado = Matematica.Soma(num1, num2);
+
+                Resultado.Text = "O resultado é " + resultado;
+            };
+
+
+            BtnSubtrai.Clicked += delegate
+            {
+                int num1 = int.Parse(Numero1.Text);
+                int num2 = int.Parse(Numero2.Text);
+
+                int resultado = Matematica.Substrai(num1, num2);
+
+                Resultado.Text = "O resultado é " + resultado;
+            };
+        }
+	}
+}
---
+++ App1/App1/logica/Matematica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.logica
+{
+    class Matematica
+    {
+        public static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substrai(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
